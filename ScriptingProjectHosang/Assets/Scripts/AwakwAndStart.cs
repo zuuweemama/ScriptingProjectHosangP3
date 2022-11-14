ae837867e8136580b7419ba17517c8c9a6eac7d5
zuu <i.tis.to.be.a.fan@gmail.com>
2022-11-14 11:05:03 -0800

    Started and Finished AwakeAndStart
    Started and finished Awake and Start Scripting Project

---
+++ ScriptingProjectHosang/Assets/Scripts/AwakwAndStart.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AwakwAndStart : MonoBehaviour
+{
+
+    public void Awake()
+    {
+        Debug.Log("Awake called.");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start Called");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
